1ff79064eae1a5fc1983affcba5b1f124d1b03d6
musfiqur552608 <mushfiqur15-10872@diu.edu.bd>
2022-02-22 09:10:13 +0600

    PagingSource & PagingData

--- app/src/main/java/com/Musfiq/imagesearchapp/data/UnsplashRepository.kt
+++ app/src/main/java/com/Musfiq/imagesearchapp/data/UnsplashRepository.kt
@@ -1,10 +1,24 @@
 package com.Musfiq.imagesearchapp.data
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.liveData
 import com.Musfiq.imagesearchapp.api.UnsplashApi
+import retrofit2.http.Query
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class UnsplashRepository @Inject constructor(private val unsplashApi: UnsplashApi) {
+
+    fun getSearchResults(query: String)=
+        Pager(
+            config = PagingConfig(
+                pageSize = 20,
+                maxSize = 100,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = {UnsplashPagingSource(unsplashApi, query)}
+        ).liveData
 
 }
--- app/src/main/java/com/Musfiq/imagesearchapp/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/Musfiq/imagesearchapp/ui/gallery/GalleryFragment.kt
@@ -1,9 +1,20 @@
 package com.Musfiq.imagesearchapp.ui.gallery
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.Musfiq.imagesearchapp.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class GalleryFragment : Fragment(R.layout.fragment_gallery){
+    private val viewModel by viewModels<GalleryViewModel>()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.photos.observe(viewLifecycleOwner){
+
+        }
+    }
 }
